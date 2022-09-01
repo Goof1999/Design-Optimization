b6d2b4ffbbbd5da4d8b470389c9026843e6cb80d
Goof1999 <59471044+Goof1999@users.noreply.github.com>
2022-08-31 23:16:33 -0700

    demo

--- HW1.py
+++ HW1.py
@@ -5,39 +5,45 @@
 # HW1 problem 1 Solution
 
 import scipy.optimize as spo
-import numpy as np
 
 
 # Define Optimization function
-def f(x1, x2, x3, x4, x5):
-    y = (x1 - x2) ** 2 - (x2 + x3 - 2) ** 2 + (x4 - 1) ** 2 + (x5 - 1) ** 2
+def f(x):
+    y = (x[0] - x[1]) ** 2 + (x[1] + x[2] - 2) ** 2 + (x[3] - 1) ** 2 + (x[4] - 1) ** 2
     return y
 
 
 # Define Constraints
-def cons1(x1, x2):
-    return x1 + x2 * 3
+def cons1(x):
+    return x[0] + x[1] * 3
 
 
-def cons2(x3, x4, x5):
-    return x3 + x4 - 2 * x5
+def cons2(x):
+    return x[2] + x[3] - 2 * x[4]
 
 
-def cons3(x2, x5):
-    return x2 - x5
+def cons3(x):
+    return x[1] - x[4]
 
-
-# Initial guess
-ini = [1, 1, 1, 1, 1]
-
-# Bounds
-b = (-10,10)
-boundry = (b,b,b,b,b)
-
+# Give constraint
 constr1 = {'type': 'eq', 'fun': cons1}
 constr2 = {'type': 'eq', 'fun': cons2}
 constr3 = {'type': 'eq', 'fun': cons3}
 constr = [constr1,constr2,constr3]
 
-solution = spo.minimize(f,ini,bounds = boundry,constraints = constr)
+# Bounds limits for all the variables
+b = [-10,10]
+boundrylimit = (b,b,b,b,b)
+
+
+# Initial guess
+guess=[5,4,3,2,1]
+
+print("Initial guess is",guess)
+
+print("The solution for the initial guess is: ")
+
+# Finally calculating the minimum value of the function
+
+solution = spo.minimize(f,guess,bounds = boundrylimit,constraints = constr)
 print(solution)
